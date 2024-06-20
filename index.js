fcf7f8e7044a7c1b4e5df4ea2bcb21d85f47004b
masoudwebprogrammer <masoudabdi36@yahoo.com>
2024-06-20 18:08:49 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const questions = require('./lib/questions.js');
+const fileName = "./examples/logo.svg";
+const setShape = require('./lib/setShape.js')
+
+
+function createLogo(response) {
+    const svg = setShape(response);
+    fs.writeFile(fileName, svg, () => console.log('Generated logo.svg'));
+}
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            createLogo(response);
+        })
+        .catch(err => {
+            console.log(err)
+        });
+}
+
+init();
